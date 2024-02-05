e1c5d6160a17492ce41ba7d71975867957d0ad67
Pásztor Márk <pasztormarkk7@gmail.com>
2024-02-05 21:31:55 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {} from 'react'
+import './App.css'
+import Navbar from './components/navbar/navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <div className="main">
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,46 @@
+import { } from 'react'
+import { FiMenu } from "react-icons/fi";
+import { useState } from 'react';
+import './navbar.css'
+
+export function Navbar() {
+    const [sideShown, setSide] = useState(true)
+    return (
+        <>
+            <nav className='UpperContainer'>
+                
+                    <ul className='NavLinks'>
+                        <div className="LogoCont">
+                            <a href="#" style={{fontSize: 80}}>
+                                <img src="https://media.discordapp.net/attachments/1200923236364456017/1204160855097483335/hugyos.png?ex=65d3b930&is=65c14430&hm=948dd67f6162116f8f8b0e91c0926c591d731c58921df087610ee98505c2d4df&=&format=webp&quality=lossless" alt="Logo" />
+                            </a>
+                        </div>
+                        <li></li>
+                        <li><a href="#" id='nlink'>Kezdőlap<hr /></a></li>
+                        <li><a href="#" id='nlink'>Galéria<hr /></a></li>
+                        <li><a href="#" id='nlink'>Árak<hr /></a></li>
+                        <li><a href="#" id='nlink'>Kapcsolat<hr /></a></li>
+                    </ul>
+            </nav>
+            <nav className='SideContainer' id={sideShown ? 'sideActive' : 'sideInactive'}>
+                <div>
+                    <div className="LogoCont">
+                        <a href="#" style={{fontSize: 100}}>
+                        <img src="https://media.discordapp.net/attachments/1200923236364456017/1204160855097483335/hugyos.png?ex=65d3b930&is=65c14430&hm=948dd67f6162116f8f8b0e91c0926c591d731c58921df087610ee98505c2d4df&=&format=webp&quality=lossless" alt="Logo" />
+                        </a>
+                    </div>
+                </div>
+                <ul className='NavLinks'>
+                        <li><a href="#" id='nlink'>Kezdőlap<hr /></a></li>
+                        <li><a href="#" id='nlink'>Galéria<hr /></a></li>
+                        <li><a href="#" id='nlink'>Árak<hr /></a></li>
+                        <li><a href="#" id='nlink'>Kapcsolat<hr /></a></li>
+                </ul>
+            </nav>
+            <div className="SidebarToggler" id={sideShown ? 'togglerActive' : 'togglerInactive'}>
+                <a onClick={() => setSide(!sideShown)}><FiMenu/></a>
+            </div>
+        </>
+    )
+}
+export default Navbar
