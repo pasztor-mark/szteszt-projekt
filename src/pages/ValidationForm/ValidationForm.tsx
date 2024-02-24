c9c9a851dda26e6317684c3722ca917e7b4b879d
Pásztor Márk <pasztormarkk7@gmail.com>
2024-02-24 23:58:34 +0100

    fix: uncalled useeffect törölve

--- src/pages/ValidationForm/ValidationForm.tsx
+++ src/pages/ValidationForm/ValidationForm.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import Navbar from '../../components/navbar/navbar'
 import Footer from '../../components/footer/Footer'
 import './ValidationForm.scss'
